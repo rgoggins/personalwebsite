f1c2f7183dbcd80eb7f38284ceeed8bca5796cf5
Ryan Goggins <ryangoggins@rgoggins.guests.local>
2019-08-19 14:55:31 -0400

    add react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('server functioning properly');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('mount')
+  );
+});
