dd8f1b6a80965feaa96a7b70332a393740b9f907
Ryan Goggins <ryangoggins@rgoggins.guests.local>
2019-08-21 12:45:21 -0400

    initial add

---
+++ src/MainClass.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/**
+ * A counter button: tap the button to increase the count.
+ */
+class MainClass extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+export default MainClass;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
+import MainClass from './MainClass';
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    React.createElement(Counter),
+    React.createElement(MainClass),
     document.getElementById('mount')
   );
 });
